cc6e668d01c97819c8cb93871ae777cf936f56b0
msvalina <marijan.svalina@gmail.com>
2013-02-22 02:23:26 +0100

    Male promjene u opisu, imenu classe, i dodan primjer postavljanja widgeta u drugi prozor

--- vetos.py
+++ vetos.py
@@ -5,13 +5,13 @@
 #raspored smjene. Moze odabrati dane i sate. 
 #takoder moze pogledati raspored samo za sebe, tj rezerviorane termine.
 
-#sucelje za  korisnika: otvori se mali upitnik s pitanjima na koja ce 
+#sucelje za korisnika(veternira): otvori se mali upitnik s pitanjima na koja ce 
 #se moci odgovoriti ili oznacavanjem kucice ili upisivanjem s tipkovnice
 #pitanja: kojeg veterinara zelite? (kucica)
 #koja je vasa zivotinja? (kucica pas, macka, itd ili upis iz tipkovnice 
 #ako je nesto sto nije navedeno)
-#
 #razlog posjeta (kucica i upis s tipkovnice)
+
 #neka od pitanja tj odgovora otvaraju nova pitanja
 #termin koji vam odgovara (mjesec dana unaprijed) i onda se bira ovisno
 #o veterinaru tj njegovoj smjeni neki od slobodnih termina koji
@@ -24,13 +24,11 @@
 from Tkinter import *
 
 
-class Example(Frame):
+class VetOs(Frame):
   
     def __init__(self, parent):
         Frame.__init__(self, parent, background="white")   
-         
         self.parent = parent
-        
         self.initUI()
         
     
@@ -46,13 +44,17 @@ class Example(Frame):
         B2.pack()
 
     def raspored(self):
-        prozor_raspored= Toplevel()
+        # The Toplevel widget is used to provide a separate window
+        # container.
+        prozor_raspored= Toplevel(bg="white")
         prozor_raspored.geometry ("800x600+400+400")
         prozor_raspored.title("Raspored")
         
+        B1 = Button(prozor_raspored, text ="Nova narudzba", command=self.narudzba)
+        B1.pack()
     
     def narudzba(self):
-         prozor_narudzba= Toplevel()
+         prozor_narudzba= Toplevel(bg="white")
          prozor_narudzba.geometry ("800x600+400+400")
          prozor_narudzba.title("Nova narudzba")
         
@@ -61,7 +63,7 @@ def main():
   
     root = Tk()
     root.geometry("800x600+300+300")
-    app = Example(root)
+    app = VetOs(root)
     root.mainloop()  
 
 
