89db4f389c8c8b8003af44d793fc6f4256655ad4
msvalina <marijan.svalina@gmail.com>
2013-02-24 12:09:02 +0100

    dodan label u prozor narudzbe i isporban unos putem StringVar

--- vetos.py
+++ vetos.py
@@ -5,7 +5,8 @@
 #raspored smjene. Moze odabrati dane i sate. 
 #takoder moze pogledati raspored samo za sebe, tj rezerviorane termine.
 
-#sucelje za korisnika(veternira): otvori se mali upitnik s pitanjima na koja ce 
+#sucelje za korisnika(veternira): 
+#upitnik s pitanjima na koja ce 
 #se moci odgovoriti ili oznacavanjem kucice ili upisivanjem s tipkovnice
 #pitanja: kojeg veterinara zelite? (kucica)
 #koja je vasa zivotinja? (kucica pas, macka, itd ili upis iz tipkovnice 
@@ -45,6 +46,7 @@ class VetOs(Frame):
         B2.pack()
         B2.place(relx=0.5, rely=0.9, anchor=CENTER)
 
+
     def raspored(self):
         # The Toplevel widget is used to provide a separate window
         # container.
@@ -58,16 +60,40 @@ class VetOs(Frame):
         B1 = Button(raspored, text ="Nova narudzba", command=self.narudzba)
         B1.pack()
         B1.place(relx=0.5, rely=0.8, anchor=CENTER)
+
     
     def narudzba(self):
         narudzba = Toplevel(bg="white")
         narudzba.geometry ("800x600+400+200")
         narudzba.title("Nova narudzba")
         
-        L1 = Label(narudzba, text="Unesi raspored")
+        L1 = Label(narudzba, text="Unesi narudzba")
         L1.pack()
         L1.place(relx=0.5, rely=0.1, anchor=CENTER)
         
+        L2 = Label(narudzba, text="Koja je vaša životinja?")
+        L2.pack()
+        L2.place(relx=0.5, rely=0.2, anchor=CENTER)
+        L3 = Label(narudzba, text="Ralog posjeta?")
+        L3.pack()
+        L3.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+        # self.entry je dostupan svim f u klasi
+        # self.entryVariable = StringVar()
+        # self.entry = Entry(narudzba,textvariable=self.entryVariable, width=50)
+        # self.entry.focus_set()
+        # self.entry.pack()
+        # self.place(relx=0.5, rely=0.35, anchor=CENTER)
+        # self.entry.bind("<Return>", self.OnPressEnter)
+        # self.entryVariable.set(u"Enter text here.")
+        # self.labelVariable = StringVar()
+        # label = Label(self,textvariable=self.labelVariable, anchor="w",fg="white",bg="blue")
+
+    # def OnPressEnter(self,event):
+    #     # self.labelVariable.set( self.entryVariable.get()+" (You pressed ENTER)" )
+    #     self.entry.focus_set()
+    #     self.entry.selection_range(0, END)
+
 def main():
   
     root = Tk()
